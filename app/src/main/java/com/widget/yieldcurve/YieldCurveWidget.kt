eeeb44d2fd37fed2817447f091634efd71d110e1
mohagan9 <mohagan9@gmail.com>
2022-01-31 17:53:36 +0000

    Chart

--- app/src/main/java/com/widget/yieldcurve/YieldCurveWidget.kt
+++ app/src/main/java/com/widget/yieldcurve/YieldCurveWidget.kt
@@ -3,11 +3,14 @@ package com.widget.yieldcurve
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
-import android.content.Intent
+import android.view.View
 import android.widget.RemoteViews
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.widget.yieldcurve.config.RetrofitConfig
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import java.text.SimpleDateFormat
 import java.util.*
@@ -41,23 +44,34 @@ internal fun updateAppWidget(
     appWidgetId: Int
 ) {
     val currentDate = SimpleDateFormat("yyyy-MM-dd", Locale.US).format(Date())
-    val currentTime = Date().time
     val disposable = RetrofitConfig.yieldCurveApi()
         .getYieldCurveSnapshot(currentDate)
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe (
             { response -> run {
+                val chart = LineChart(context)
+                val entries = listOf(
+                    Entry(0.3F, response[0].yield_3m),
+                    Entry(2F, response[0].yield_2y),
+                    Entry(5F, response[0].yield_5y),
+                    Entry(10F, response[0].yield_10y),
+                    Entry(30F, response[0].yield_30y)
+                )
+                val dataSet = LineDataSet(entries, "US Treasury Yield Curve")
+                chart.data = LineData(dataSet)
+                chart.measure(
+                    View.MeasureSpec.makeMeasureSpec(500,View.MeasureSpec.EXACTLY),
+                    View.MeasureSpec.makeMeasureSpec(500,View.MeasureSpec.EXACTLY))
+                chart.layout(0, 0, chart.measuredWidth, chart.measuredHeight)
+
                 val views = RemoteViews(context.packageName, R.layout.yield_curve_widget)
-                views.setTextViewText(R.id.appwidget_text, "$currentTime $response")
+                views.setImageViewBitmap(R.id.chart_image, chart.chartBitmap)
 
                 appWidgetManager.updateAppWidget(appWidgetId, views)
             } },
             { error -> run {
-                val views = RemoteViews(context.packageName, R.layout.yield_curve_widget)
-                views.setTextViewText(R.id.appwidget_text, error.localizedMessage ?: "Error")
 
-                appWidgetManager.updateAppWidget(appWidgetId, views)
             } }
         )
 }
